79fe3265a8573c070624721fc66a6b07d68e4395
beth-codes <70582711+beth-codes@users.noreply.github.com>
2024-04-04 19:05:15 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,17 +4,18 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('employees.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('employees.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-// const router = jsonServer.router('db.json')
+const router = jsonServer.router('employees.json')
 
 const middlewares = jsonServer.defaults()
+console.log('global runnig', router)
 
 server.use(middlewares)
 // Add this before server.use(router)
